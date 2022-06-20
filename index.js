ba68734f10e91b9bc021c3afc037cbe3141ade14
Laura Bobadilla <69031844+lkbobadi@users.noreply.github.com>
2022-06-20 16:34:49 -0400

    css mobile structure complete
    Most of the CSS matches. Need to focus on background-color properties for 'Thank-you' class.
    
    Need to add :active psuedoselectors.

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 var thankYouCard = document.querySelector(".thank-you");
-thankYouCard.style.display = "none";
+thankYouCard.style.display = "block";
+
+var ratingQuestion = document.querySelector(".rating-question");
+ratingQuestion.style.display = "none";
