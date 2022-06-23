d3d050fe4cc9f1d4cf393ef3ee1148a53179dd2c
Laura Bobadilla <69031844+lkbobadi@users.noreply.github.com>
2022-06-22 22:17:35 -0400

    Added JS - Rating CSS Changes
    Completed the CSS change for "listItems" loop in the Rating system

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
+// Thank You Card will change after hitting submit button
+
 var thankYouCard = document.querySelector(".thank-you");
-thankYouCard.style.display = "block";
+thankYouCard.style.display = "none";
 
 var ratingQuestion = document.querySelector(".rating-question");
-ratingQuestion.style.display = "none";
+ratingQuestion.style.display = "block";
+
+var submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", showThankYou);
+
+function showThankYou() {
+  setTimeout(() => {
+    ratingQuestion.style.display = "none";
+    thankYouCard.style.display = "block";
+  }, 350);
+}
+
+// End of thank you card
+
+// Looping through a list of Rating Items to add specific CSS
+
+var listItems = document.querySelectorAll(".rate");
+
+for (var i = 0; i < listItems.length; i++) {
+  document.querySelectorAll(".rate")[i].addEventListener("click", function () {
+    var currentRateButton = this;
+
+    listItems.forEach((listItem) => {
+      listItem.classList.remove("active");
+      currentRateButton.classList.add("active");
+    });
+  });
+}
+
+// End of looping through the list
